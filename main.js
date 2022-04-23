fbbade84e295ca9031617050c0c6d561fb8885b9
elgiz93 <98798356+elgiz93@users.noreply.github.com>
2022-04-23 11:57:46 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+var siz=0;
+var qarsi=0;
+rock.onclick= function(){
+   let random=Math.floor(Math.random() * 3);
+   console.log(random);
+   document.getElementById("siz").src="das1.jpg"
+   if(random==0){
+      document.getElementById("cavab").innerHTML="YENİDƏN SEÇİN!"
+      document.getElementById("cavab").style.color="black"
+      document.getElementById("qarsi").src="das2.jpg"
+   }
+   else if(random==1){
+      document.getElementById("cavab").innerHTML="MƏĞLUB OLDUNUZ!"
+      document.getElementById("qarsi").src="yarpaq2.jpg"
+      document.getElementById("cavab").style.color="gray"
+      qarsi++;
+   }
+   else if(random==2){
+      document.getElementById("cavab").innerHTML="QAZANDINIZ!"
+      document.getElementById("cavab").style.color="red"
+      document.getElementById("qarsi").src="qayci2.jpg"
+      siz++;
+   }
+   document.getElementById("netice").innerHTML=`${siz}:${qarsi}`
+}
+paper.onclick= function(){
+   let random=Math.floor(Math.random() * 3);
+   console.log(random);
+   document.getElementById("siz").src="yarpaq1.jpg"
+   if(random==0){
+      document.getElementById("cavab").innerHTML="QAZANDINIZ!"
+      document.getElementById("cavab").style.color="red"
+      document.getElementById("qarsi").src="das2.jpg"
+      siz++;
+   }
+   else if(random==1){
+      document.getElementById("cavab").innerHTML="YENİDƏN SEÇİN!"
+      document.getElementById("cavab").style.color="black"
+      document.getElementById("qarsi").src="yarpaq2.jpg"
+   }
+   else if(random==2){
+      document.getElementById("cavab").innerHTML="MƏĞLUB OLDUNUZ!"
+      document.getElementById("cavab").style.color="gray"
+      document.getElementById("qarsi").src="qayci2.jpg"
+      qarsi++;
+   }
+   document.getElementById("netice").innerHTML=`${siz}:${qarsi}`
+}
+qayci.onclick= function(){
+   let random=Math.floor(Math.random() * 3);
+   console.log(random);
+   document.getElementById("siz").src="qayci1.jpg"
+   if(random==0){
+      document.getElementById("cavab").innerHTML="MƏĞLUB OLDUNUZ!"
+      document.getElementById("cavab").style.color="gray"
+      document.getElementById("qarsi").src="das2.jpg"
+      qarsi++;
+   }
+   else if(random==1){
+      document.getElementById("cavab").innerHTML="QAZANDINIZ!"
+      document.getElementById("qarsi").src="yarpaq2.jpg"  
+      document.getElementById("cavab").style.color="red"
+      siz++;
+    }
+   else if(random==2){
+      document.getElementById("cavab").innerHTML="YENİDƏN SEÇİN!"
+      document.getElementById("cavab").style.color="black"
+      document.getElementById("qarsi").src="qayci2.jpg"
+   }
+   document.getElementById("netice").innerHTML=`${siz}:${qarsi}`
+}
